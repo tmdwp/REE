3caaf2bf9e7a2def75263b78269592c3d185743f
pakrsujin <momopetit813@gmail.com>
2025-02-04 10:44:09 +0900

    아이템 추가
    추가 수정

--- Program.cs
+++ Program.cs
@@ -53,7 +53,7 @@
                 }
             }
 
-            public class player
+            public class Player : ICharacter
             {
                 public int Level { get; set; }
                 public string Name { get; }
@@ -170,7 +170,7 @@
 
                       input = -1;
                     } 
-                }while (input != 0);
+                }
             }
             
             public interface Item
@@ -206,7 +206,107 @@
                     Descript = descript;
                     IsEquip = false;
                 }
+
+                public void Equip(Player player)
+                {
+                    switch (Category)
+                    {
+                        case (int)ItemCategory.Weapon ;
+                            if (IsEquip == false)
+                            {
+                                player.Attack += Stat;
+                                player.EquipAttack += Stat;
+                                IsEquip = true;
+                            }
+                            else
+                            {
+                                player.Attack -= Stat;
+                                player.EquipAttack -= Stat;
+                                IsEquip = false;
+                            }
+                            break;
+                        case (int)ItemCategory.chaos ;
+                            if (IsEquip == false)
+                            {
+                                player.Attack = new Random().Next(-100, 100);
+                                player.Defense = new Random().Next(-100, 100);
+                                IsEquip = true;
+                            }
+                            else
+                            {
+                                player.Attack = new Random().Next(-100, 100);
+                                player.Defense = new Random().Next(-100, 100);
+                                IsEquip = false;
+                            }
+                            break;
+                    }
+                }
             }
+
+            public void Inform()
+            {
+                if (IsEquip == true)
+                {
+                    Console.WriteLine("[E]");
+                }
+
+                switch (Category)
+                {
+                    case (int)ItemCategory.Weapon:
+                        Console.WriteLine(Name + " | 공격력 +" + Stat + " | " + Descript);
+                        break;
+                    case (int)ItemCategory.Armor:
+                        Console.WriteLine(Name + " | 방어력 +" + Stat + " | " + Descript);
+                        break;
+                    case (int)ItemCategory.chaos:
+                        Console.WriteLine(Name + " | ???? | " + Descript);
+                        break;
+                }
+            }
+            
+            public class ShopItem : Item
+            {
+                public string Name { get; set; }
+                public int Category { get; set; }
+                public int Stat { get; set; }
+                public string Descript { get; set; }
+                public bool IsBuy;
+                public int Price;
+
+                public ShopItem(string name, int category, int stat, string descript, int price)
+                {
+                    Name = name;
+                    Category = category;
+                    Stat = stat;
+                    Descript = descript;
+                    IsBuy = false;
+                    Price = price;
+                }
+
+                public void Inform()
+                {
+                    switch (Category)
+                    {
+                        case (int)ItemCategory.Weapon:
+                            Console.Write(Name + " | 공격력 +" + Stat + " | " + Descript);
+                            break;
+                        case (int)ItemCategory.Armor:
+                            Console.Write(Name + " | 방어력 +" + Stat + " | " + Descript);
+                            break;
+                        case (int)ItemCategory.chaos:
+                            Console.Write(Name + " | ? | " + Descript);
+                            break;   
+                    }
+
+                    if (IsBuy)
+                    {
+                        Console.WriteLine(" | 구매 완료!");
+                    }
+                    else Console.WriteLine(" | " + Price + "G");
+                }
+               
+            }
+
             public class Shop
             {
                 private List<ShopItem> shopList;
@@ -226,9 +326,129 @@
                     shopList.Add(new ShopItem("훈련용 검", (int)ItemCategory.Weapon, 5, "훈련용이지만 날이 서있는 검이다.", 1200));
                     shopList.Add(new ShopItem("매우 무거운 검", (int)ItemCategory.Weapon, 15, "많이 무거운 검이다. 다치지 않게 조심하자.", 2300));
                 }
-                
+
+                public void ShopMenu(Player player)
+                {
+                    int count = 1;
+                    do
+                    {
+                        Console.Clear();
+                        Console.WriteLine("\n-------------------------------------------\n");
+                        Console.WriteLine("\t\t상점");
+                        Console.WriteLine("\n-------------------------------------------");
+
+                        foreach (ShopItem item in shopList)
+                        {
+                            Console.WriteLine("-" + count + "");
+                            item.Inform();
+                            count++;
+                        }
+
+                        ShopBuy(player);
+                        if (buy != 0)
+                            break;
+                        count = 1;
+                    } while (buy != 0);
+                }
+
+                public void ShopBuy(Player player) // 구매!!!
+                {
+                    Console.WriteLine("\n-------------------------------------------\n");
+                    Console.WriteLine("현재 G : " + player.Gold + "G\n");
+                    Console.WriteLine("구매할 아이템을 선택하세요 ( 0 - 나가기 | 1 ~ " + shopList.Count + " - 구매할 아이템 번호)");
+                    while (!int.TryParse(Console.ReadLine(), out buy))
+                    {
+                        Console.WriteLine("잘못된 입력입니다.");
+                        Console.WriteLine("구매할 아이템을 선택하세요 ( 0 - 나가기 | 1 ~ " + shopList.Count + " - 구매할 아이템 번호)");
+                    }
+
+                    if (buy > 0 && buy <= shopList.Count)
+                    {
+                        buy--;
+                        if (!shopList[buy].IsBuy)
+                        {
+                            if (player.Gold >= shopList[buy].Price)
+                            {
+                                ShopItem selected = shopList[buy];
+                                player.Gold -= selected.Price;
+                                MyItem buyitem = new MyItem(selected.Name, selected.Category, selected.Stat, selected.Descript);
+                                player.Inventory.Add(player.Inventory.Count, buyitem);
+                                selected.IsBuy = true;
+                                
+                                Console.WriteLine("아이템을 구매하였습니다.");
+                                Console.WriteLine("현재 G : " + player.Gold + "G\n");
+                            }
+                            else
+                            {
+                                Console.WriteLine("가지고 있는 골드가 부족해요!!");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("이미 가지고 있는 아이템인데요!");
+                        }
+
+                        buy++;
+                        Console.ReadLine();
+                    }
+                    else if (buy !=0)
+                    {
+                        Console.WriteLine("없는 아이템입니다!!!");
+                    }
+                }
+
             }
-        }
+            
+            // 마을
+            public class Town
+            {
+                public void Start(Player player, Shop shop)
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("\n-------------------------------------------");
+                        Console.WriteLine("\t여기는 마을입니다.");
+                        Console.WriteLine("던전에 들어가기 전 행동을 할 수 있습니다.\n");
+                        Console.WriteLine("1. 상태 보기");
+                        Console.WriteLine("2. 인벤토리");
+                        Console.WriteLine("3. 상점\n");
+                        Console.WriteLine("원하는 행동을 선택해주세요.(1~3)");
+                        int move; //선택한 행동
+                        while (!int.TryParse(Console.ReadLine(), out move))
+                        {
+                            Console.WriteLine("정해진 행동의 번호가 아닙니다.");
+                            Console.WriteLine("원하는 행동을 선택해주세요.(1~3)");
+                        }
+
+                        switch (move)
+                        {
+                            case 1 ;
+                                Console.Clear();
+                                player.StatusOpen();
+                                break;
+                            case 2 ;
+                                Console.Clear();
+                                player.OpenInventory();
+                                break;
+                            case 3 ;
+                                Console.Clear();
+                                shop.ShopMenu(player);
+                                break;
+                            case 1010 ;
+                                player.Gold += 50000;
+                                Console.WriteLine("test");
+                                break;
+                            default:
+                                Console.WriteLine("틀렸습니다.");
+                                Console.WriteLine("게임을 종료합니다.");
+                                Console.WriteLine("안녕히 가세요.\n\n");
+                                return;
+                        }
+                    }
+                }
+            }
+            
+        } 
     }
     
 }
